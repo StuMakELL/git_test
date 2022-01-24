3539bcec945aebdbc11d289338f8c9cd1dcc861d
StuMak <s.makridis@gmail.com>
2022-01-24 03:35:25 +0200

    dev commit
    Signed-off-by: StuMak <s.makridis@gmail.com>

--- main.c
+++ main.c
@@ -2,17 +2,21 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+static void dev_func()
+{
+}
+
 static void system_initialize()
 {
-
 }
 
 int main()
 {
     system_initialize();
-    
-    while(true)
+
+    while (true)
     {
+        dev_func();
     }
     return 0;
 }
